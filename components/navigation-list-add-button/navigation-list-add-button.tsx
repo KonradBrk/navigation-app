d7e74f17f7ecc2c1f30ea94013ba39998b9cadcb
Konrad Brylak <konrad.brk@gmail.com>
2024-12-01 18:48:01 +0100

    feat: adding navigation list add button

---
+++ components/navigation-list-add-button/navigation-list-add-button.tsx
@@ -0,0 +1,18 @@
+type NavigationListAddButtonProps = {
+  onClick: () => void;
+};
+
+export const NavigationListAddButton = ({
+  onClick,
+}: NavigationListAddButtonProps) => {
+  return (
+    <div className="p-4 bg-gray-100 border-t border-gray-200 rounded-b-md">
+      <button
+        onClick={onClick}
+        className="px-4 py-2 text-sm text-gray-700 bg-white border border-gray-300 hover:bg-gray-200"
+      >
+        Dodaj pozycję menu
+      </button>
+    </div>
+  );
+};
